21ea10d017e2b32bab6dca91380e221b7252f1c4
Amoslove <loveamos83@gmail.com>
2023-05-18 04:53:49 -0700

    my portfolio

---
+++ 3d-circular-progress-bar/README.md
@@ -0,0 +1,4 @@
+# 3D circular progress bar
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mortezasharifinia/pen/BaVZZeM](https://codepen.io/mortezasharifinia/pen/BaVZZeM).
+
---
+++ style.js
@@ -0,0 +1,12 @@
+  //  function myFucntion(){
+  //    if(document.getElementById("btn").innerHTML==='light mode'){
+  //     document.body.style.background='#fff';
+  //     document.body.style.color="#333";
+  //     document.getElementById("btn").innerHTML='light mode';
+  //   }
+  //   else{
+  //     document.body.style.background='yellow';
+  //     document.body.style.color='#333';
+  //     document.getElementById('btn').innerHTML='darkmode';
+  //   }
+  //   }
